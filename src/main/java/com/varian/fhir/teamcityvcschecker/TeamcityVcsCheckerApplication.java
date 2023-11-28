1d73c4277ecd8ca0ef2461a31a0be34638d2751f
Harish Lanke <harish.lanke@varian.com>
2023-11-29 00:54:02 +0530

    Initial commit

---
+++ src/main/java/com/varian/fhir/teamcityvcschecker/TeamcityVcsCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.varian.fhir.teamcityvcschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityVcsCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcityVcsCheckerApplication.class, args);
+    }
+
+}
